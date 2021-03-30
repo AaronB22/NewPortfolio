1dccf5577117d799a237751e48efac0ac8652793
Aaron <aaronthesnowboarder@live.com>
2021-03-29 22:21:14 -0700

    Styled ad postion the projects. Added links to Github, linkenIn, and my deployed projects.

---
+++ script.js
@@ -0,0 +1,15 @@
+export const projOne = document.querySelector("#mainProject");
+projOne.addEventListener("click", toProj);
+const weatherApp = document.querySelector("#leftBlock");
+weatherApp.addEventListener("click", goToWeatherApp);
+const dayPlannerApp = document.querySelector("#rightBlock");
+dayPlannerApp.addEventListener("click", goToDayPlanner);
+function toProj() {
+    window.location.replace("https://aaronb22.github.io/Project_1/");
+}
+function goToWeatherApp() {
+    window.location.replace("https://aaronb22.github.io/WeatherHW/");
+}
+function goToDayPlanner() {
+    window.location.replace("https://aaronb22.github.io/DayPlanner/");
+}
